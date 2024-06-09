83eaa7f8cf13745d7b4535e6f7fba04cfa0e45f8
arben-djokovic <benidjokovic12@gmail.com>
2024-06-09 16:45:27 +0200

    error fixed

--- src/components/home-nav/HomeNav.js
+++ src/components/home-nav/HomeNav.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './HomeNav.scss'
+import {auth} from '../../services/AuthService'
 
 export default function HomeNav() {
+  const isLogged = auth.getAuthStatus()
   return (
     <nav className='homeNav'>
         <ul>
             <li><a href="/">All Songs</a></li>
             <li><a href="/playlists">All Playlists</a></li>
-            <li><a href="/my-playlists">My Playlists</a></li>
+            {isLogged && <li><a href="/my-playlists">My Playlists</a></li>}
         </ul>
     </nav>
   )
